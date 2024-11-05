e2966d568e850405919500f33e2e54c4526e5e47
Chrysa Polymenakou <chrysapol2808@gmail.com>
2024-11-05 18:03:03 +0200

    trolley

--- trolley/src/trolley.c
+++ trolley/src/trolley.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
   int main(){
-      long long int a=1/(1e18);
-      long long int b=1/(1e18);
-      for(a<1e18; b<1e18;){
-    printf("Please enter the cost of going left:");
-    scanf("%lld", &a);
+      long long int a=-1e18;
+      long long int b=-1e18;
+
+     do  {
+      printf("Please enter the cost of going left:");   
+           char c = scanf("%lld", &a);
+      
+      if (c == EOF ) {
+          printf("<Found EOF>\n");
+          return 0;
+      }
+      
+     if  (( a < -1e18 ) || (a > 1e18 ))
+          return 1;
+
      printf("Please enter the cost of going right:");
-    scanf("%lld", &b);
-      if (a<=b)
-    printf("Go left.\n");
-      if (a>b)
-    printf("Go right.\n");
-   }
- return 0;
+         char d = scanf("%lld", &b);
+
+         if ( d == EOF) {
+               printf("No right cost provided\n");
+               return 1;
+         }
+
+      if  (( b < -1e18 ) || (b > 1e18 ))
+          return 1;
+
+      if(a<=b)
+          printf("Go left.\n");
+      if(a>b)
+         printf("Go right.\n");
+   }  while (1);
+ 
+  return 0;
 }
       
